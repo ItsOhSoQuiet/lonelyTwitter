a58600db145ca6c3913c3c4a6db155ee10701041
ItsOhSoQuiet <mkluk@ualberta.ca>
2017-09-13 20:01:14 -0600

    Created mood abstract base class

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/CurrentMood.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/CurrentMood.java
@@ -10,5 +10,26 @@ public abstract class CurrentMood {
     private String mood;
     private Date date;
 
+    public CurrentMood(String mood){
+        date = new Date();
+        this.mood = mood;
+    }
 
+    public CurrentMood(String mood, Date date){
+        this.date = date;
+        this.mood = mood;
+    }
+
+    public String getMood(){ return mood; }
+    public Date getDate(){ return date; }
+
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public abstract String moodType();
 }
