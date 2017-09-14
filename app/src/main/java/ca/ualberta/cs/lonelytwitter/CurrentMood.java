6a7f9a4811fd635debaaeda74fc6d381cc52cc0d
ItsOhSoQuiet <mkluk@ualberta.ca>
2017-09-13 19:23:07 -0600

    Started a mood abstract class

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/CurrentMood.java
@@ -0,0 +1,14 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by mkluk on 9/13/17.
+ */
+
+public abstract class CurrentMood {
+    private String mood;
+    private Date date;
+
+
+}
